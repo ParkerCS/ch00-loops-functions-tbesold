544f61dd2f17800aa99a600329e7176f6e25ac3f
tbesold <tbesold@fwparker.org>
2017-01-31 19:47:09 -0600

    comment

--- ch00_problem_set(FUNCTIONS).py
+++ ch00_problem_set(FUNCTIONS).py
@@ -1,10 +1,20 @@
 #SECTION 2 - FUNCTIONS (20PTS TOTAL)
 
+import math
+import random
+
 #PROBLEM 1 (Length of String - 3pts)
 # Make a function which asks the user to enter a string, then prints the length of that string.
 # You will need to use the input() function.
 # Make a call to that function
 
+def function():
+    user_input = input("write something: ")
+    length = len(user_input)
+    print(length)
+function()
+
+import math
 # PROBLEM 2 (Pythagorean theorem - 4pts)
 # The Pythagorean theorem states that of a right triangle, the square of the
 # length of the diagonal side is equal to the sum of the squares of the lengths
@@ -13,19 +23,68 @@
 # Then calculate the length of the third side, and display it in a nicely formatted way.
 # You may ignore the fact that the user can enter negative or zero lengths for the sides.
 
+def pythag(a,b):
+    c_squared = a**2 + b**2
+    c = math.sqrt(c_squared)
+
+    print("the length of the hypotenuse is", c)
+a = int(input("enter a value for the side of a right triangle: "))
+b = int(input("enter another value for the side of the right triangle: "))
+pythag(a,b)
+
 # PROBLEM 3 (Biggest, smallest, average - 4pts)
 # Make a function to ask the user to enter three numbers.
 # Then print the largest, the smallest, and their average, rounded to 2 decimals.
 # Display the answers in a "nicely" formatted way.
 # Make a call to your function.
 
+def numbers(a,b,c):
+    if a >= b and a >= c:
+        print(a, "is the largest number you entered")
+    if b > a and b >= c:
+        print(b, "is the largest number you entered")
+    if c > a and c > b:
+        print(c, "is the largest number you entered")
+    if a <= b and a <= c:
+        print(a, "is the smallest number you entered")
+    if b < a and b <= c:
+        print(b, "is the smallest number you entered")
+    if c < a and c < b:
+        print(c, "is the smallest number you entered")
+    average = (float(a + b + c) / 3)
+    print("the average of the numbers is", float(round(average,2)))
+
+a = int(input("enter a number: "))
+b = int(input("enter another number: "))
+c = int(input("enter one more number: "))
+numbers(a,b,c)
+
 # PROBLEM 4 (e to the... - 3pts)
 # Calculate the value of e (from the math library) to the power of -1, 0, 1, 2, and 3.
 # display the results, with 5 decimals, in a nicely formatted manner.
+
+
+def e():
+    print(round(math.e ** -1, 5))
+    print(round(math.e ** 0, 5))
+    print(round(math.e ** 1, 5))
+    print(round(math.e ** 2, 5))
+    print(round(math.e ** 3, 5))
+e()
 
 # PROBLEM 5 (Random int - 3pts)
 # Generate a random integer between 1 and 10 (1 and 10 both included),
 # but only use the random() function (randrange is not allowed here)
 
+random_int = int(random.random() * 10)
+print("the random number from 1 to 10 is", random_int)
+
 # PROBLEM 6 (add me, multiply me - 3pts)
 # Make a function which takes in two integers and RETURNS their sum AND their product.
+
+def function(a, b):
+    sum = a + b
+    product = a * b
+    return sum, product
+print(function(10,3))
+
--- ch00_problem_set(LOOPS).py
+++ ch00_problem_set(LOOPS).py
@@ -1,5 +1,5 @@
 # LOOPS (22pts TOTAL)
-
+import random
 # PROBLEM 1 (Fibonacci - 4pts)
 ## The Fibonacci sequence is a sequence of numbers that starts with 1, followed by 1 again.
 # Every next number is the sum of the two previous numbers.
@@ -7,23 +7,71 @@
 # Write a program that calculates and prints the Fibonacci sequence
 # until the numbers get higher than 1000.
 
+number = 0
+number_2 = 1
+number_3 = 1
+
+while number <= 1000:
+    number_3 = number + number_2
+    number += number_3
+
+    print(number_3)
+
+#stuck here not sure what to do
+
+
+
+
 
 # PROBLEM 2 (Number Guessing Game - 6pts)
 # Write a program that takes a random integer between 1 and 1000
 # The program then asks the user to guess the number.
-# After every guess, the program will say either “Lower”
-# if the number it took is lower, “Higher” if the number it took is higher,
-# and “You guessed it!” if the number it took is equal to the number
+
 # It might be wise, for testing purposes, to also display the number that the
 # program randomly picks, until you are sure that the program works correctly
+
+random_int = random.randrange(1,1001)
+for i in range(500):
+    user_guess = int(input("Guess a number between one and a thousand: "))
+    if user_guess > random_int:
+        print("your guess is higher than the number. Try again")
+    if user_guess < random_int:
+        print("your guess is lower than the number. Try again")
+    if user_guess == random_int:
+        print("You guessed correctly!")
+        break
+
+
+
+
 
 # PROBLEM 3 (Dice Hi-Low - 6pts)
 # You roll five six-sided dice, one by one.
 # How big is the probability that the value of each die
 # is greater than or equal to the value of the previous die that you rolled?
-# For example, the sequence “1, 1, 4, 4, 6” is a success,
-# but “1, 1, 4, 3, 6” is not. Determine the
+
 # probability of success using a simulation of a large number of trials.
+
+success = 0
+rolls = 100000
+for i in range(rolls):
+    a = random.randrange(1,7)
+    b = random.randrange(1,7)
+    c = random.randrange(1,7)
+    d = random.randrange(1,7)
+    e = random.randrange(1,7)
+    if a <= b and b <= c and c <= d and d <= e:
+        success += 1
+#print(success/rolls)   not sure why this doesnt work
+print("of", rolls, "rolls", success, "were successful")
+
+
+
+
+
+
+
+
 
 # PROBLEM 4 (Number Puzzler - 6pts)
 # A, B, C, and D are all different digits.
@@ -31,4 +79,19 @@
 # What are the digits?
 # Note: to make ABCD and DCBA conventional numbers, neither A nor D can be zero.
 # Use a quadruple-nested loop to solve.
+
+for a in range(1,10):
+    for b in range(10):
+        for c in range(10):
+            for d in range(1,10):
+                ABCD = int(str(a) +str(b) + str(c) + str(d))
+                DCBA = int(str(d) + str(c) + str(b) + str(a))
+                if DCBA == 4 * ABCD:
+                    print("ABCD is", a, b, c, d)
+                    print("DCBA is", d, c, b, a)
+
+
+
+
+
 
